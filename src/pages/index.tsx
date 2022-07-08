d790c7fec7a205b0dc5e22a447bac7b33af55f03
fabioborges <fabioborgesouzajunior@gmail.com>
2022-07-08 16:27:44 -0300

    button subscribe adicionado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import styles from "./home.module.scss"
+import { SubscribeButton } from "../components/SubscribeButton/index"
 
 export default function Home() {
   return (
@@ -16,10 +17,11 @@ export default function Home() {
           <p>Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.jpeg" alt="Girl Coding" />
-        
+
       </main>
     </>
   )
